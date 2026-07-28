03e00b8dc60ef00a3ab4ab85350a6ceb31d8c23c
Sebastiaan van Stijn <github@gone.nl>
2026-07-28 10:09:09 +0200

    archive: minor godoc touch-ups
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- time_nonwindows.go
+++ time_nonwindows.go
@@ -14,10 +14,11 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// chtimes changes the access time and modified time of a file at the given path.
-// If the modified time is prior to the Unix Epoch (unixMinTime), or after the
-// end of Unix Time (unixEpochTime), os.Chtimes has undefined behavior. In this
-// case, Chtimes defaults to Unix Epoch, just in case.
+// chtimes changes the access and modification time of a file at the given
+// path.
+//
+// Callers must use boundTime to ensure timestamps are within the range
+// supported by os.Chtimes.
 func chtimes(name string, atime time.Time, mtime time.Time) error {
 	return os.Chtimes(name, atime, mtime)
 }
